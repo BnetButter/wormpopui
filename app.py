03aa02bc270a0512a4609b9aa7ecc03697fcc876
Yug Patel <ybpkrk@mst.edu>
2024-02-15 16:32:02 -0600

    simulation can properly run, cannot run multiple simulation instances yet

--- app.py
+++ app.py
@@ -1,56 +1,49 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request
 import subprocess
 import json
-import uuid
-from threading import Thread
 import os
-from flask import render_template
 
 app = Flask(__name__)
 
-simulations = {}
-
 @app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/get-defaults', methods=['GET'])
-def get_defaults():
+def form():
     with open('simulation/constants.json') as f:
-        constants = json.load(f)
-    return jsonify(constants)
+        default_params = json.load(f)
+    return render_template('index.html', default_params=default_params)
 
-def start_simulation_process(sim_id, parameters):
-    parameters_path = f'simulation/tmp_{sim_id}.json'
-    with open(parameters_path, 'w') as f:
+@app.route('/run-simulation', methods=['POST'])
+def run_simulation():
+    parameters = request.form.to_dict()
+    directory_name = parameters.pop('directory')  
+    database_name = parameters.pop('database')  
+    
+    
+    with open('simulation/constants.json') as f:
+        types = json.load(f)
+    for key, value in parameters.items():
+        if isinstance(types[key], int):
+            parameters[key] = int(value)
+        elif isinstance(types[key], float):
+            parameters[key] = float(value)
+    
+    temp_params_file = 'temp_constants.json'
+    with open(temp_params_file, 'w') as f:
         json.dump(parameters, f)
     
-    command = ['python3', 'simulation/wormpop.py', f'--parameters={parameters_path}']
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    command = [
+        'python', 'simulation/wormpop.py',
+        '--parameters=' + temp_params_file,
+        '--database=' + database_name,
+        '--directory=' + directory_name
+    ]
+    
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, cwd=os.getcwd())
     stdout, stderr = process.communicate()
-    
-    simulations[sim_id] = {'status': 'Completed', 'output': stdout.decode()}
-    
-    os.remove(parameters_path)
-    # Save the output to a results file
-    with open(f'simulation/results_{sim_id}.txt', 'w') as f:
-        f.write(stdout.decode())
 
-@app.route('/new-simulation', methods=['POST'])
-def new_simulation():
-    data = request.json
-    sim_id = str(uuid.uuid4())
-    Thread(target=start_simulation_process, args=(sim_id, data)).start()
-    return jsonify({'simulation_id': sim_id})
-
-@app.route('/simulation-details/<simulation_id>', methods=['GET'])
-def simulation_details(simulation_id):
-    try:
-        with open(f'simulation/results_{simulation_id}.txt') as f:
-            results = f.read()
-        return jsonify({'results': results})
-    except FileNotFoundError:
-        return jsonify({'error': 'Results not found'}), 404
+    response = f'<pre>{stdout}</pre>'
+    if stderr:
+        response += f'<pre>Error: {stderr}</pre>'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)

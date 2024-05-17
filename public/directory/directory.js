284ce34f2988944d93a43647dfc3204709fc1342
Kevin Lai <zlnh4@umsystem.edu>
2024-05-17 15:35:31 -0500

    graph for individual

--- public/directory/directory.js
+++ public/directory/directory.js
@@ -5,20 +5,29 @@ function SimulationInstance(guid, name) {
     self.selected = ko.observable(false)
 }
 
+function SelectableHeader(name, observableX) {
+    const self = this
+    self.name = name
+    self.observableX = observableX
+    self.selected = ko.observable(false)
+    self.selected.subscribe(() => {
+        if (self.selected && self.name === self.observableX()) {
+            self.selected(false)
+        }
+    })
+}
+
 function TableModel(data, headers) {
     const self = this
     self.headers = headers
     self.rows = []
-
-    console.log(self.headers[0])
-    
-    const numRows = data[self.headers[0]].length
+    const numRows = data[self.headers[0].name].length
     
     for (let i = 0; i < numRows; i++) {
         const row = []
         
         for (let col of self.headers) {
-            const column = data[col]
+            const column = data[col.name]
             row.push(column[i])
         }
         self.rows.push(row)
@@ -33,15 +42,58 @@ function DirectoryModel() {
     self.selectedSimulation = ko.observable(null)
 
     self.sqlQuery = ko.observable("HGELLO")
+    self.sqlResults = ko.observable(null)
+
     self.tableView = ko.observable(false)
+
+    self.tableModel = ko.observable()
 
     self.tableFields = ko.observableArray([])
     self.tableError = ko.observable("")
 
+    /* Controls if we show table or the graph */
+    self.showTable = ko.observable(true)
+    
+    self.tableLoaded = ko.computed(() => {
+        const table = self.tableView()
+        if (!table) return false 
+        return table.headers.length > 0
+    })
+
+    self.showTable.subscribe((v) => {
+        console.log(v)
+    })
+
+    self.toggleShowTable = () => {
+        self.showTable(! self.showTable())
+    }
+
+    /* Pagination */
     self.rowsPerPage = ko.observable(50)
     self.pageNumber = ko.observable(1)
 
+    /* Graphable Columns */
+    self.selectedX = ko.observable("")
+
+    /* Make sure selected X and Y can't be the same header  */
+    self.selectedY = () => self.tableModel() 
+        ? self.tableModel().headers
+            .filter((x) => x.selected()) 
+            .map((x) => x.name)
+            .filter((x) => x !== self.selectedX())
+        : []
     
+        /* Make sure selected X and Y can't be the same header  */
+    self.selectedX.subscribe((value) => {
+        console.log(value)
+        self.tableModel().headers.forEach((header) => {
+            const name = header.name
+            if (name === value) {
+                header.selected(false)
+            }
+        })
+    })
+
     // Load simulation instance
     fetch('/api/job-status')
         .then(response => response.json())
@@ -107,18 +159,63 @@ function DirectoryModel() {
         .then(data => {
             // Handle the data from the server
             // This function handles the JSON data returned from the server
-            
-            self.tableFields(new TableModel(data.result, data.header))
-        
+            console.log(data)
+            self.sqlResults(data.result)
+            self.tableModel(new TableModel(data.result, data.header.map((name) => new SelectableHeader(name, self.selectedX))))
+            self.tableFields(self.tableModel())
         })
         .catch(error => {
             // Handle any errors that occurred during the fetch
             console.error('There was a problem with the fetch operation:', error);
+            self.sqlResults(null)
         });
     }
 
     self.rowsPerPage.subscribe(self.executeSql)
     self.pageNumber.subscribe(self.executeSql)
+
+
+    self.showTable.subscribe((showTable) => {
+        const sqlResults = self.sqlResults()
+        if (showTable)
+            return
+
+        console.log(self.selectedY(), )
+
+
+        console.log(sqlResults)
+
+        var layout = {
+            title: 'Line and Scatter Plot',
+            xaxis: {
+                title: self.selectedX()
+            },
+            yaxis: {
+                title: self.selectedY()[0]
+            }
+        };
+
+        const Xaxis = sqlResults[self.selectedX()]
+
+        const Yaxis = sqlResults[self.selectedY()[0]]
+
+
+        var trace1 = {
+            x: Xaxis,
+            y: Yaxis,
+            mode: 'lines+markers',
+            type: 'scatter'
+        };
+
+        var data = [trace1];
+
+        Plotly.newPlot('myDiv', data, layout)
+
+        
+        
+    })
+
+
 }
 
 // Apply bindings only after the DOM is ready
